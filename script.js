12f2fb39e2956fe1a39ec61110f5903a61a9c270
manishapaudel <manishapaudel093@gmail.com>
2024-09-18 09:42:58 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+//let first=document.getElementsByClassName("first") [1]
+//console.log("first",first)
+//first.style.backgroundColor="red";
+
+let hi = document.getElementById("hi")
+hi.style.background = "green";
